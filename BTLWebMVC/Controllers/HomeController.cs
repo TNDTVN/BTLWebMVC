64a5dee04670d08052f2622813eae9083599ba18
hao <dhao30167@gmail.com>
2025-01-14 11:13:31 +0700

    c

--- BTLWebMVC/Controllers/HomeController.cs
+++ BTLWebMVC/Controllers/HomeController.cs
@@ -3,26 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using BTLWebMVC.App_Start;
+using System.Web.Helpers;
 
 namespace BTLWebMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Context _context;
         public ActionResult Index()
         {
+
             return View();
         }
+        [HttpPost]
+        public ActionResult KiemTra(string username, string password)
+        {
+            var taiKhoan = _context.Accounts.FirstOrDefault(a => a.Username == username && a.Password == password);
+            if (taiKhoan == null) 
+            {
+                if (taiKhoan.Role == "Admin" || taiKhoan.Role == "Employee")
+                {
 
+                }
+                else if(taiKhoan.Role == "Customer")
+                { 
+                
+                }
+            }
+
+            return View("Index");
+        }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
