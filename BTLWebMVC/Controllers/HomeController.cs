998d0cac532760b37e022a1446fd1b961f927698
hao <dhao30167@gmail.com>
2025-01-16 19:03:17 +0700

    v

--- BTLWebMVC/Controllers/HomeController.cs
+++ BTLWebMVC/Controllers/HomeController.cs
@@ -11,50 +11,66 @@ namespace BTLWebMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly Context _context;
+        private Context db = new Context();
 
-        public HomeController()
+        public ActionResult Index(int? id)
         {
-            _context = new Context(); 
-        }
-        public ActionResult Index()
-        {
-
             return View();
         }
-        [HttpPost]
-        public ActionResult KiemTra(string username, string password)
-        {
-            var taiKhoan = _context.Accounts.FirstOrDefault(a => a.Username == username && a.Password == password);
-
-            if (taiKhoan == null)
-            {
-                TempData["ErrorMessage"] = "Tên đăng nhập hoặc mật khẩu không đúng!";
-                ViewBag.ShowSignInModal = true; 
-                return View("Index");
-            }
-
-            Session["AccountId"] = taiKhoan.AccountID;
-            Session["Role"] = taiKhoan.Role;
-
-            if (taiKhoan.Role == "Admin" || taiKhoan.Role == "Employee")
-            {
-                return RedirectToAction("Index");
-            }
-            else if (taiKhoan.Role == "Customer")
-            {
-                return RedirectToAction("Index", "Customer");
-            }
-
-            return RedirectToAction("Index");
-        }
-
         public ActionResult About()
         {
 
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Check(string username, string password)
+        {
+            var account = db.Accounts.FirstOrDefault(a => a.Username == username && a.Password == password);
 
+            if (account == null)
+            {
+                TempData["ErrorMessage"] = "Tên đăng nhập hoặc mật khẩu không đúng!";
+                return View("Login");
+            }
+
+            Session["AccountId"] = account.AccountID;
+            Session["Role"] = account.Role;
+            TempData["SuccessMessage"] = "Bạn đã đăng nhập thành công!";
+            if (account.Role == "Admin" || account.Role == "Employee")
+            {
+                return RedirectToAction("Index", "HomeManager", new { id = account.AccountID }); 
+            }
+            else if (account.Role == "Customer")
+            {
+                return RedirectToAction("Index", new { id = account.AccountID });
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Quyền không hợp lệ!";
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+
+            TempData["SuccessMessage"] = "Bạn đã đăng xuất thành công!";
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Forgotpassword()
+        {
+            return View();
+        }
         public ActionResult Contact()
         {
 
---
+++ BTLWebMVC/Controllers/HomeManagerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTLWebMVC.Controllers
+{
+    public class HomeManagerController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            return View();
+        }
+    }
+}
