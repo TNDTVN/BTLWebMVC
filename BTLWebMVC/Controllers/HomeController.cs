20ddeb2ac8ddd760ae2050a239b13fa6ffaaf92b
hao <dhao30167@gmail.com>
2025-02-13 13:59:07 +0700

    v

--- BTLWebMVC/Controllers/HomeController.cs
+++ BTLWebMVC/Controllers/HomeController.cs
@@ -8,18 +8,28 @@ using BTLWebMVC.App_Start;
 using System.Web.Helpers;
 using System.Threading.Tasks;
 using BTLWebMVC.Models;
+using PagedList;
 
 namespace BTLWebMVC.Controllers
 {
     public class HomeController : Controller
     {
+
         private Context db = new Context();
 
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            var products = db.Products.Include(p => p.Images).Take(50).ToList();
-            return View(products); 
+            if (page == null) page = 1;
+
+            var products = db.Products.Include(p => p.Images).Include(p => p.Category)
+                                      .OrderByDescending(x => x.ProductID);
+            var categories = db.Categories.ToList(); 
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            ViewBag.Categories = categories;
+            return View(products.ToPagedList(pageNumber, pageSize));
         }
+
 
         public ActionResult About()
         {
