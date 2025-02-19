5957f72a824e7a26146b1127da9d4565e5bb01cf
hao <dhao30167@gmail.com>
2025-02-19 13:29:52 +0700

    c

--- BTLWebMVC/Controllers/Manager/HomeManagerController.cs
+++ BTLWebMVC/Controllers/Manager/HomeManagerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BTLWebMVC.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,10 @@ namespace BTLWebMVC.Controllers
 {
     public class HomeManagerController : Controller
     {
+        private Context db = new Context();
         public ActionResult Index(int? id)
         {
+
             return View();
         }
     }
