6ad363062376469f15b700bf41e7edc188924220
Hào <dhao30167@gmail.com>
2025-04-01 18:33:16 +0700

    contact

--- BTLWebMVC/Scripts/contact_custom.js
+++ BTLWebMVC/Scripts/contact_custom.js
@@ -1,4 +1,4 @@
-/* JS Document */
+﻿/* JS Document */
 
 /******************************
 
@@ -6,354 +6,128 @@
 
 1. Vars and Inits
 
-
-
 ******************************/
 
-jQuery(document).ready(function($)
-{
-	"use strict";
+jQuery(document).ready(function ($) {
+    "use strict";
 
-	/* 
+    var header = $('.header');
+    var topNav = $('.top_nav');
+    var hamburger = $('.hamburger_container');
+    var menu = $('.hamburger_menu');
+    var menuActive = false;
+    var hamburgerClose = $('.hamburger_close');
+    var fsOverlay = $('.fs_menu_overlay');
 
-	1. Vars and Inits
+    setHeader();
 
-	*/
+    $(window).on('resize', function () {
+        setHeader();
+    });
 
-	var header = $('.header');
-	var topNav = $('.top_nav')
-	var hamburger = $('.hamburger_container');
-	var menu = $('.hamburger_menu');
-	var menuActive = false;
-	var hamburgerClose = $('.hamburger_close');
-	var fsOverlay = $('.fs_menu_overlay');
-	var map;
+    $(document).on('scroll', function () {
+        setHeader();
+    });
 
-	setHeader();
+    initMenu();
+    initMap();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
+    function setHeader() {
+        if (window.innerWidth < 992) {
+            if ($(window).scrollTop() > 100) {
+                header.css({ 'top': "0" });
+            } else {
+                header.css({ 'top': "0" });
+            }
+        } else {
+            if ($(window).scrollTop() > 100) {
+                header.css({ 'top': "-50px" });
+            } else {
+                header.css({ 'top': "0" });
+            }
+        }
+        if (window.innerWidth > 991 && menuActive) {
+            closeMenu();
+        }
+    }
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+    function initMenu() {
+        if (hamburger.length) {
+            hamburger.on('click', function () {
+                if (!menuActive) {
+                    openMenu();
+                }
+            });
+        }
 
-	initMenu();
-	initGoogleMap();
+        if (fsOverlay.length) {
+            fsOverlay.on('click', function () {
+                if (menuActive) {
+                    closeMenu();
+                }
+            });
+        }
 
-	function setHeader()
-	{
-		if(window.innerWidth < 992)
-		{
-			if($(window).scrollTop() > 100)
-			{
-				header.css({'top':"0"});
-			}
-			else
-			{
-				header.css({'top':"0"});
-			}
-		}
-		else
-		{
-			if($(window).scrollTop() > 100)
-			{
-				header.css({'top':"-50px"});
-			}
-			else
-			{
-				header.css({'top':"0"});
-			}
-		}
-		if(window.innerWidth > 991 && menuActive)
-		{
-			closeMenu();
-		}
-	}
+        if (hamburgerClose.length) {
+            hamburgerClose.on('click', function () {
+                if (menuActive) {
+                    closeMenu();
+                }
+            });
+        }
 
-	function initMenu()
-	{
-		if(hamburger.length)
-		{
-			hamburger.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-			});
-		}
+        if ($('.menu_item').length) {
+            var items = document.getElementsByClassName('menu_item');
+            var i;
 
-		if(fsOverlay.length)
-		{
-			fsOverlay.on('click', function()
-			{
-				if(menuActive)
-				{
-					closeMenu();
-				}
-			});
-		}
+            for (i = 0; i < items.length; i++) {
+                if (items[i].classList.contains("has-children")) {
+                    items[i].onclick = function () {
+                        this.classList.toggle("active");
+                        var panel = this.children[1];
+                        if (panel.style.maxHeight) {
+                            panel.style.maxHeight = null;
+                        } else {
+                            panel.style.maxHeight = panel.scrollHeight + "px";
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-		if(hamburgerClose.length)
-		{
-			hamburgerClose.on('click', function()
-			{
-				if(menuActive)
-				{
-					closeMenu();
-				}
-			});
-		}
+    function openMenu() {
+        menu.addClass('active');
+        fsOverlay.css('pointer-events', "auto");
+        menuActive = true;
+    }
 
-		if($('.menu_item').length)
-		{
-			var items = document.getElementsByClassName('menu_item');
-			var i;
+    function closeMenu() {
+        menu.removeClass('active');
+        fsOverlay.css('pointer-events', "none");
+        menuActive = false;
+    }
 
-			for(i = 0; i < items.length; i++)
-			{
-				if(items[i].classList.contains("has-children"))
-				{
-					items[i].onclick = function()
-					{
-						this.classList.toggle("active");
-						var panel = this.children[1];
-					    if(panel.style.maxHeight)
-					    {
-					    	panel.style.maxHeight = null;
-					    }
-					    else
-					    {
-					    	panel.style.maxHeight = panel.scrollHeight + "px";
-					    }
-					}
-				}	
-			}
-		}
-	}
+    function initMap() {
+        var latitude = mapData.latitude;
+        var longitude = mapData.longitude;
+        var locationName = mapData.locationName;
 
-	function openMenu()
-	{
-		menu.addClass('active');
-		// menu.css('right', "0");
-		fsOverlay.css('pointer-events', "auto");
-		menuActive = true;
-	}
+        var map = L.map('map').setView([latitude, longitude], 16);
 
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		fsOverlay.css('pointer-events', "none");
-		menuActive = false;
-	}
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            maxZoom: 18,
+        }).addTo(map);
 
-	function initGoogleMap()
-	{
-		var myLatlng = new google.maps.LatLng(42.373122,-71.112387);
-    	var mapOptions = 
-    	{
-    		center: myLatlng,
-	       	zoom: 16,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			draggable: true,
-			scrollwheel: false,
-			zoomControl: true,
-			zoomControlOptions:
-			{
-				position: google.maps.ControlPosition.RIGHT_CENTER
-			},
-			mapTypeControl: false,
-			scaleControl: false,
-			streetViewControl: false,
-			rotateControl: false,
-			fullscreenControl: true,
-			styles:
-			[
-			  {
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#f5f5f5"
-			      }
-			    ]
-			  },
-			  {
-			    "elementType": "labels.icon",
-			    "stylers": [
-			      {
-			        "visibility": "off"
-			      }
-			    ]
-			  },
-			  {
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#616161"
-			      }
-			    ]
-			  },
-			  {
-			    "elementType": "labels.text.stroke",
-			    "stylers": [
-			      {
-			        "color": "#f5f5f5"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "administrative.land_parcel",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#bdbdbd"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "poi",
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#eeeeee"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "poi",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#757575"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "poi.park",
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#e5e5e5"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "poi.park",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#9e9e9e"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "road",
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#ffffff"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "road.arterial",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#757575"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "road.highway",
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#dadada"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "road.highway",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#616161"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "road.local",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#9e9e9e"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "transit.line",
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#e5e5e5"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "transit.station",
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#eeeeee"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "water",
-			    "elementType": "geometry",
-			    "stylers": [
-			      {
-			        "color": "#c9c9c9"
-			      }
-			    ]
-			  },
-			  {
-			    "featureType": "water",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#9e9e9e"
-			      }
-			    ]
-			  }
-			]
-    	}
+        var marker = L.marker([latitude, longitude]).addTo(map);
+        marker.bindPopup(locationName).openPopup();
 
-    	// Initialize a map with options
-    	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    	// Use an image for a marker
-		var image = 'images/map_marker.png';
-		var marker = new google.maps.Marker(
-		{
-			position: new google.maps.LatLng(42.373122,-71.112387),
-			map: map,
-			icon: image
-		});
-
-		// Re-center map after window resize
-		google.maps.event.addDomListener(window, 'resize', function()
-		{
-			setTimeout(function()
-			{
-				google.maps.event.trigger(map, "resize");
-				map.setCenter(myLatlng);
-			}, 1400);
-		});
-	}
+        window.addEventListener('resize', function () {
+            setTimeout(function () {
+                map.invalidateSize();
+                map.setView([latitude, longitude], 16);
+            }, 1400);
+        });
+    }
 });
