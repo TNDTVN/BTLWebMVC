b3fa53547af0232a0c42faafeb0137fe5c5e19b3
Huong002 <psymint002@gmail.com>
2025-05-15 19:05:57 +0700

    sdsd

---
+++ BTLWebMVC/App_Start/CustomAuthorizeAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BTLWebMVC.Models;
+using System.Web.Mvc;
+
+namespace BTLWebMVC.App_Start
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        // han chế các chức năn theo quyền
+
+        private readonly string[] allowedRoles;
+        public CustomAuthorizeAttribute(params string[] roles)
+        {
+            allowedRoles = roles;
+
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var accountId = httpContext.Session["AccountId"]?.ToString();
+            if (string.IsNullOrEmpty(accountId))
+            {
+                return false;
+            }
+
+            int id = Convert.ToInt32(accountId);
+            using (var db = new Context())
+            {
+                var account = db.Accounts.FirstOrDefault(a => a.AccountID == id);
+                if (account == null)
+                {
+                    return false;
+                }
+                return allowedRoles.Contains(account.Role);
+            }
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                new System.Web.Routing.RouteValueDictionary
+                {
+                    { "controller", "Home" },
+                    { "action", "AccessDenied" }
+                });
+        }
+    }
+}
